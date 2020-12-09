100a41bc5fd94500328f4cfe2f4d8357f4add76f
AnnaBIB201 <75685295+AnnaBIB201@users.noreply.github.com>
2020-12-09 11:51:33 +0500

    Проверила нажатие на круги
    Реализовывала добавление новых кругов после нажатия на старые. Нужно убрать наложение кругов друг на друга

--- Proect.py
+++ Proect.py
@@ -18,6 +18,8 @@ min_r = 10
 max_r = 60
 max_circles = 13
 count = 0
+circles = []
+press = (0,0)
 
 # инициация,
 # создаем объектов
@@ -28,7 +30,7 @@ clock = pygame.time.Clock()
 # для отображения до цикла
 # какиех-то объектов, обновляем экран
 pygame.display.update()
-sc.fill(PINK)
+
 # главный цикл
 while True:
 
@@ -39,16 +41,26 @@ while True:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             sys.exit()
+        if i.type == pygame.MOUSEBUTTONDOWN:
+            press = i.pos
 
     # TODO
     # изменяем объекты
 
     # обновляем экран
-    # заливаем экран белым
+    # заливаем экран розовым
+    sc.fill(PINK)
     while count< max_circles:
         x = random.randint(0, max_w)
         y = random.randint(0, max_h)
         r = random.randint(min_r, max_r)
-        pygame.draw.circle(sc, YELLOW, (x, y), r)
+        circles.append((x,y,r)) # сохраняем созданные круги в списке
         count += 1
+    for i in circles:
+        if (press[0]-i[0])**2+(press[1]-i[1])**2<= i[2]**2: #проверяем принадлежность координат нажатия координатам круга
+            circles.remove(i)
+            count-=1 #вместо осчезнувшего круга добавляем новый
+    for i in circles:
+        pygame.draw.circle(sc, YELLOW, (i[0], i[1]), i[2]) #рисуем кружочки
+
     pygame.display.update()
