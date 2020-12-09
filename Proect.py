acb66c3c36bf6a32e46e9dfe7ea64ed13949df1c
arya-shsh <75690128+arya-shsh@users.noreply.github.com>
2020-12-09 09:30:35 +0300

    Добавила графику
    Изменила цвет рабочей области, добавила кружочки, настроила их цвет. Нужно сделать так, чтобы кружочки отображались каждый кадр!

--- Proect.py
+++ Proect.py
@@ -1,36 +1,54 @@
-#подключаем модули
+# подключаем модули
 import pygame
 import sys
- 
-#определяем константы,
-#классы и функции
+import random
+
+# определяем константы,
+# классы и функции
 FPS = 60
- 
-#инициация,
-#создаем объектов
+PINK = (255, 110, 199)
+YELLOW = (252, 247, 135)
+
+
+
+
+max_w = 600
+max_h = 400
+min_r = 10
+max_r = 60
+max_circles = 13
+count = 0
+
+# инициация,
+# создаем объектов
 pygame.init()
-sc = pygame.display.set_mode((600, 400))
+sc = pygame.display.set_mode((max_w, max_h))
 clock = pygame.time.Clock()
- 
-#для отображения до цикла
-#какиех-то объектов, обновляем экран
+
+# для отображения до цикла
+# какиех-то объектов, обновляем экран
 pygame.display.update()
- 
-#главный цикл
+sc.fill(PINK)
+# главный цикл
 while True:
- 
-    #задержка
+
+    # задержка
     clock.tick(FPS)
- 
-    #цикл для обработки событий
+
+    # цикл для обработки событий
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             sys.exit()
- 
-    #TODO
-    #изменяем объекты
 
-    
- 
-    #обновляем экран
+    # TODO
+    # изменяем объекты
+
+    # обновляем экран
+    # заливаем экран белым
+    while count< max_circles:
+        x = random.randint(0, max_w)
+        y = random.randint(0, max_h)
+        r = random.randint(min_r, max_r)
+        pygame.draw.circle(sc, YELLOW, (x, y), r)
+        count += 1
     pygame.display.update()
