65c378725e31a4bf8beac1d3d372939392b6330c
AnnaBIB201 <75685295+AnnaBIB201@users.noreply.github.com>
2020-12-16 17:50:21 +0500

    Создала класс кружочков
    В классе присутствует функция их создания и проверки на наличие пересечения

---
+++ shapes.py
@@ -0,0 +1,14 @@
+class Circle():
+    def __init__(self,x,y,r,color,vx,vy):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.color = color
+        self.vx = vx
+        self.vy = vy
+    def peresechenie(self,other):
+        if (self.x - other.x)**2 + (self.y - other.y)**2<=(self.r + other.r)**2:
+            return True
+        else:
+            return False
+
