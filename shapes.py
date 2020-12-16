55404e436dee0fd6566bdda1b8e56fbe4da51f7b
arya-shsh <75690128+arya-shsh@users.noreply.github.com>
2020-12-16 17:44:46 +0300

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,12 +1,13 @@
 class Circle():
-    def __init__(self,x,y,r,color,vx,vy):
+    def init(self,x,y,r,color,vx,vy):
         self.x = x
         self.y = y
         self.r = r
         self.color = color
+        self.vx = vx
+        self.vy = vy
     def peresechenie(self,other):
-        if (self.x - other.x)**2 + (self.y - other.y)**2<=(self.r + other.r)**2:
+        if (self.x - other.x)**2 + (self.y - other.y)**2 <=(self.r + other.r)**2:
             return True
         else:
             return False
-
