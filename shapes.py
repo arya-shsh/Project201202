428692621242efb4e3ea55236b25ac8549f54979
AnnaBIB201 <75685295+AnnaBIB201@users.noreply.github.com>
2020-12-17 21:16:49 +0500

    Добавила класс прямоугольников

--- shapes.py
+++ shapes.py
@@ -1,5 +1,5 @@
 class Circle():
-    def init(self,x,y,r,color,vx,vy):
+    def __init__(self,x,y,r,color,vx,vy):
         self.x = x
         self.y = y
         self.r = r
@@ -7,7 +7,16 @@ class Circle():
         self.vx = vx
         self.vy = vy
     def peresechenie(self,other):
-        if (self.x - other.x)**2 + (self.y - other.y)**2 <=(self.r + other.r)**2:
+        if (self.x - other.x)**2 + (self.y - other.y)**2<=(self.r + other.r)**2:
             return True
         else:
             return False
+class Rect():
+    def __init__(self,x,y,w,h,color,vx,vy):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.color = color
+        self.vx = vx
+        self.vy = vy
